33a4d07ca6cb40f8db4e70ab8a3ac0d736e849b7
pratyush-20 <114431331+pratyush-20@users.noreply.github.com>
2022-11-12 00:39:58 +0530

    Add files via upload

---
+++ django/Hello/home/views.py
@@ -0,0 +1,17 @@
+from http.client import HTTPResponse
+from django.shortcuts import render,HttpResponse
+
+
+def index(request):
+  return render (request,'index.html')
+
+def about (request ):
+    return render (request,'about.html')
+ 
+def services(request ):
+    return render (request,'services.html')
+   
+def contact(request ):
+    return render (request,'contact.html')
+ 
+    
